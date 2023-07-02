c9ba015f8b377e6c177f4cf4aa416d9e1c320300
Johnson Phan <phantruongson96@gmail.com>
2023-07-02 17:12:07 -0400

    cart item pop up modal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
 
       <main>
+        {/* <Cart /> */}
         <Meals />
       </main>
     </Fragment>
